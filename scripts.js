f80fcd61ee183894e3b3313786e9b5b9e6b46bb9
Uxue Anabitarte <u.anabitarte@gmail.com>
2024-06-26 18:41:31 +0200

    mostrar ultima busqueda

--- scripts.js
+++ scripts.js
@@ -25,6 +25,34 @@ function loadDestacados() {
     })
     
 }
+function loadLastSearch(){
+    let qInput = sessionStorage.getItem('qInput')
+    let filter = sessionStorage.getItem('filter')
+    let printType = sessionStorage.getItem('printType')
+    let author = sessionStorage.getItem('author')
+    let publisher = sessionStorage.getItem('publisher')
+    let totalItems = sessionStorage.getItem('totalItems')
+    let innerHTML;
+    if(qInput) {
+        innerHTML = '<div class="container-fluid bg-light p-4 rounded mb-3"><div class="row justify-content-center"><div class="col-md-7">Texto: ' + qInput + '</div>'
+        if(filter){
+            innerHTML += '<div class="col-md-7">Buscar: ' + filter + '</div>'
+        }
+        if(printType){
+            innerHTML += '<div class="col-md-7">Tipo de libro: ' + printType + '</div>'
+        }
+        if(author){
+            innerHTML += '<div class="col-md-7">Autor: ' + author + '</div>'
+        }
+        if(publisher){
+            innerHTML += '<div class="col-md-7">Editor: ' + publisher + '</div>'
+        }
+        innerHTML += '<div class="col-md-7 fw-bold">Número de resultados: ' + totalItems + '</div></div></div>'
+    } else {
+        innerHTML = '<p class="alert alert-light text-center">¡Vaya! No tienes búsquedas recientes</p>';
+    }
+    document.getElementById('last-search').innerHTML = innerHTML;
+}
 document.addEventListener('DOMContentLoaded', () => {
   
     const loginForm = document.getElementById('loginForm')
@@ -62,28 +90,42 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 
     const searchForm = document.getElementById('searchForm')
+    function cleanLastSearch() {
+        sessionStorage.removeItem('qInput')
+        sessionStorage.removeItem('filter')
+        sessionStorage.removeItem('printType')
+        sessionStorage.removeItem('author')
+        sessionStorage.removeItem('publisher')
+        sessionStorage.removeItem('totalItems')
+    }
     if(searchForm){
         searchForm.addEventListener('submit', (e) => {
             e.preventDefault()
+            cleanLastSearch()
             let qInput = document.getElementById('qInput').value
             let filter = document.getElementById('filter').value
             let printType = document.getElementById('printType').value
             let author = document.getElementById('author').value
             let publisher = document.getElementById('publisher').value
+            sessionStorage.setItem('qInput', qInput)
 
             qInput = qInput.replaceAll(' ', '+')
             let url = 'https://www.googleapis.com/books/v1/volumes?q=' + qInput
 
             if(author) {
+                sessionStorage.setItem('author', author)
                 url += '+inauthor:' + author.replaceAll(' ', '+')
             }
             if(publisher) {
+                sessionStorage.setItem('publisher', publisher)
                 url += '+inpublisher:' + publisher.replaceAll(' ', '+')
             }
             if(filter) {
+                sessionStorage.setItem('filter', filter)
                 url += '&filter=' + filter
             }
             if(printType) {
+                sessionStorage.setItem('printType', printType)
                 url += '&printType=' + printType
             }
             url += '&maxResults=18';
@@ -91,6 +133,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 .then(response => response.json())
                 .then(result => {
                     document.getElementById("search-content").innerHTML = '';
+                    sessionStorage.setItem('totalItems', result.totalItems)
                     if(result.totalItems == 0){
                         document.getElementById("search-content").innerHTML = '<p class="text-center">No ha habido resultados para tu búsqueda</p>';
                     }
